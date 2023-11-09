974a95eba4e4c7a18b326b5da9719e89527c4767
OnoderaYuuma <y.onodera.sys23@morijyobi.ac.jp>
2023-11-09 10:48:07 +0900

    コードの追加
    Main.javaに２人分の計算ができるコードを追加

--- Main.java
+++ Main.java
@@ -3,13 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("身長を入力してください(メートル)：");
+        System.out.print("1人目の身長を入力してください(メートル)：");
         double height = Double.parseDouble(sc.nextLine());
-        System.out.print("体重を入力してください(kg)：");
+        System.out.print("１人目の体重を入力してください(kg)：");
         double weight = Double.parseDouble(sc.nextLine());
 
         double result = weight / (height * height);
-        System.out.print("あなたのBMI：");
+        System.out.print("１人目のBMI：");
+        System.out.print(String.format("%.2f",result));
+
+        System.out.print("２人目の身長を入力してください(メートル)：");
+        double height = Double.parseDouble(sc.nextLine());
+        System.out.print("２人目の体重を入力してください(kg)：");
+        double weight = Double.parseDouble(sc.nextLine());
+
+        double result = weight / (height * height);
+        System.out.print("２人目のBMI：");
         System.out.print(String.format("%.2f",result));
     }
 }
