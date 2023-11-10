e18434f08142bb40a179f20a726a214ce132ca04
OnoderaYuuma <y.onodera.sys23@morijyobi.ac.jp>
2023-11-10 11:21:11 +0900

    ファイル名の変更
    ファイルの名前を変更し、それに対応したコードを修正

--- Bmi.java
+++ Bmi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Bmi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("1人目の身長を入力してください(メートル)：");
@@ -10,14 +10,14 @@ public class Main {
 
         double result = weight / (height * height);
         System.out.print("１人目のBMI：");
-        System.out.print(String.format("%.2f",result));
+        System.out.println(String.format("%.2f",result));
 
         System.out.print("２人目の身長を入力してください(メートル)：");
-        double height = Double.parseDouble(sc.nextLine());
+        height = Double.parseDouble(sc.nextLine());
         System.out.print("２人目の体重を入力してください(kg)：");
-        double weight = Double.parseDouble(sc.nextLine());
+        weight = Double.parseDouble(sc.nextLine());
 
-        double result = weight / (height * height);
+        result = weight / (height * height);
         System.out.print("２人目のBMI：");
         System.out.print(String.format("%.2f",result));
     }
